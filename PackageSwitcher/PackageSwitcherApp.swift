2d062ef2067065f91b36b1530100f1f263ab20fc
BlackRockCity <murph2@hotmail.com>
2024-07-02 21:23:36 -0600

    Initial Commit

---
+++ PackageSwitcher/PackageSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageSwitcherApp.swift
+//  PackageSwitcher
+//
+//  Created by Chris Murphy on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PackageSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
